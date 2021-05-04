5ed53d8875af06368d71219c99be005d7663abd9
genggam090124 <64142150+genggam090124@users.noreply.github.com>
2021-05-04 20:02:10 +0700

    Create MoResponse.java

---
+++ MoResponse.java
@@ -0,0 +1,34 @@
+package com.genggam.projekfilm;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MoResponse {
+    @SerializedName("Response")
+    private String response;
+    @SerializedName("totalResults")
+    private String totalResults;
+    @SerializedName("Search")
+    private List<SearchItem> search;
+
+    public String getResponse(){
+        return response;
+    }
+
+    public void setTotalResults(String totalResults){
+        this.totalResults = totalResults;
+    }
+
+    public String getTotalResults(){
+        return totalResults;
+    }
+
+    public void setSearch(List<SearchItem> search){
+        this.search = search;
+    }
+
+    public List<SearchItem> getSearch(){
+        return search;
+    }
+}
